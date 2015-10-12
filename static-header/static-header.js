bf731ac42280dd29d9302b37f254534c0a8b5833
Noam Cohen <noam@ecb.co.il>
2015-10-12 16:24:13 +0300

    inherit td style into cell div

--- static-header/static-header.js
+++ static-header/static-header.js
@@ -23,7 +23,7 @@
 
 
                 $(this).find("td").wrapInner(function(){
-                    return "<div class='cell-wrapper' style='width:"+$(this).width()+"px'></div>";
+                    return "<div class='cell-wrapper' style='width:"+$(this).width()+"px;padding:inherit;margin:inherit'></div>";
                 });
 
                 $(this).wrap('<div class="static-header-wrapper"></div>');
