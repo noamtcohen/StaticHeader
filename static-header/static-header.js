e8f4d8b0e39a8227ade0a2135ac95089df9f339d
Noam Cohen <noam@ecb.co.il>
2015-10-15 12:32:07 +0300

    Alignment is perfect but text is not resizing to fill td

--- static-header/static-header.js
+++ static-header/static-header.js
@@ -5,7 +5,9 @@
     var PLUGIN_NAME = "staticHeader";
 
     $(window).resize(function(){
-        methods.resize();
+        $(".static-header-wrapper").each(function(){
+            methods.resize.apply(this);
+        })
     });
 
     var methods = {
@@ -13,93 +15,96 @@
             var settings = $.extend({
                 overflow:{
                     maxHeight: 300
-                },
-                tableWidth:"100%"
+                }
             }, options );
 
-            this.each(function() {
-                if($(this).data("static-header-init"))
-                    return;
-                $(this).data("static-header-init",{});
+            if($(this).data("static-header-init"))
+                return;
+            $(this).data("static-header-init",{});
 
-                $(this).width(0);
-                $(this).find("td").wrapInner(function(){
-                    return "<div class='cell-wrapper' style='width:"+$(this).width()+"px'></div>";
-                });
-                
-                $(this).wrap('<div class="static-header-wrapper"></div>');
+            $(this).wrap("<div class='static-header-tmp-wrapper' style='width:0px;'></div>");
 
-                $(this).width(settings.tableWidth);
-
-                var $thead = $(this).find("thead").detach();
-                var $tbody = $(this).find("tbody").detach();
-
-                $(this).parent().append($(this).clone());
-
-                $(this).parent().append('<div class="static-header-scroller"><div class="static-header-scroll-content">&nbsp;</div></div>');
-                var $headTbl = $(this).parent().find("table:first");
-                var $bodyTbl = $(this).parent().find("table:nth-child(2)");
-
-                $headTbl.append($thead);
-                $bodyTbl.append($tbody);
-
-                $headTbl.wrap('<div class="static-header-thead-wrapper"></div>');
-                $bodyTbl.wrap('<div class="static-header-tbody-wrapper"></div>');
-
-                var $mainWrapper = $(this).parent().parent();
-
-                $mainWrapper.find(".static-header-tbody-wrapper").css(settings.overflow);
-
-                $mainWrapper.find(".static-header-scroller").on("scroll",function(){
-                    var scrollLeft = $(this).scrollLeft();
-
-                    $mainWrapper.find(".static-header-thead-wrapper").scrollLeft(scrollLeft);
-                    $mainWrapper.find(".static-header-tbody-wrapper").scrollLeft(scrollLeft);
-                });
+            $(this).find("td").wrapInner(function(){
+                return "<div class='cell-wrapper'><div style='width:"+$(this).width()+"px'></div></div>";
             });
 
-            methods.resize();
+            $(this).wrap('<div class="static-header-wrapper"></div>');
+
+            var $thead = $(this).find("thead").detach();
+            var $tbody = $(this).find("tbody").detach();
+
+
+
+            $(this).parent().append($(this).clone());
+
+            $(this).parent().append('<div class="static-header-scroller"><div class="static-header-scroll-content">&nbsp;</div></div>');
+            var $headTbl = $(this).parent().find("table:first");
+            var $bodyTbl = $(this).parent().find("table:nth-child(2)");
+
+            $headTbl.attr("data-static-header-options",JSON.stringify(options));
+
+            $headTbl.append($thead);
+            $bodyTbl.append($tbody);
+
+            $headTbl.wrap('<div class="static-header-thead-wrapper"></div>');
+            $bodyTbl.wrap('<div class="static-header-tbody-wrapper"></div>');
+
+            var $mainWrapper = $(this).parent().parent();
+
+            $mainWrapper.find(".static-header-tbody-wrapper").css(settings.overflow);
+
+            $mainWrapper.find(".static-header-scroller").on("scroll",function(){
+                var scrollLeft = $(this).scrollLeft();
+
+                $mainWrapper.find(".static-header-thead-wrapper").scrollLeft(scrollLeft);
+                $mainWrapper.find(".static-header-tbody-wrapper").scrollLeft(scrollLeft);
+                });
+
+
+            $(this).parents('.static-header-tmp-wrapper:first').contents().unwrap();
+
+            methods.resize.apply(this);
 
             return this;
         },
         resize : function( ) {
-            $(".static-header-wrapper").each(function(){
-                var bodyTblWidth = $(this).find(".static-header-tbody-wrapper table").width();
-                $(this).find(".static-header-thead-wrapper table").width(bodyTblWidth);
-                $(this).find(".static-header-scroll-content").width(bodyTblWidth);
-                $(this).find(".static-header-scroller").width($(this).find(".static-header-tbody-wrapper").width())
-            });
+            var $wrapper = methods.findWrapper.apply(this);
+            $wrapper.find(".static-header-scroll-content").width($wrapper.find("table:first").width());
         },
         destroy:function(){
-            this.each(function() {
-                var $wrapper;
-                if($(this).hasClass("static-header-wrapper"))
-                    $wrapper = $(this);
-                else
-                    $wrapper= $(this).parents(".static-header-wrapper:first");
+            var $wrapper = methods.findWrapper.apply(this);
 
-                var $tbl = $wrapper.find(".static-header-thead-wrapper table").detach();
-                var $tbody = $wrapper.find(".static-header-tbody-wrapper table tbody").detach();
+            var $tbl = $wrapper.find(".static-header-thead-wrapper table").detach();
+            var $tbody = $wrapper.find(".static-header-tbody-wrapper table tbody").detach();
 
-                $tbl.append($tbody);
+            $tbl.append($tbody);
 
-                $tbl.removeData('static-header-init');
+            $tbl.removeData('static-header-init');
 
-                $tbl.find(".cell-wrapper").contents().unwrap();
-                $wrapper.replaceWith($tbl);
-            });
+            $tbl.find(".cell-wrapper").contents().unwrap();
+            $wrapper.replaceWith($tbl);
+
             return this;
+        },
+        findWrapper:function(){
+            if($(this).hasClass("static-header-wrapper"))
+                return $(this);
+            else
+                return $(this).parents(".static-header-wrapper:first");
         }
     };
 
     $.fn[PLUGIN_NAME] = function(methodOrOptions) {
-        if ( methods[methodOrOptions] ) {
-            return methods[ methodOrOptions ].apply( this, Array.prototype.slice.call( arguments, 1 ));
-        } else if ( typeof methodOrOptions === 'object' || ! methodOrOptions ) {
-            return methods.init.apply( this, arguments );
-        } else {
-            $.error( 'Method ' +  methodOrOptions + ' does not exist on jQuery.'+PLUGIN_NAME);
-        }
+        this.each(function() {
+            if (methods[methodOrOptions]) {
+                return methods[methodOrOptions].apply(this, Array.prototype.slice.call(arguments, 1));
+            } else if (typeof methodOrOptions === 'object' || !methodOrOptions) {
+                return methods.init.apply(this, arguments);
+            } else {
+                $.error('Method ' + methodOrOptions + ' does not exist on ' + PLUGIN_NAME + " plugin");
+            }
+        });
+        return this;
     };
 }(jQuery));
 
